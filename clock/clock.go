74829623ac01014a0e5d02dbc31cb6695133a981
Nolan Kovacik <noltron000@gmail.com>
2019-01-31 18:38:41 -0800

    ⌚ clock works and passes

--- clock/clock.go
+++ clock/clock.go
@@ -1,17 +1,90 @@
 package clock
 
-func CreateClock() {
+import (
+	"strconv"
+)
 
+type Clock struct {
+	digital string
+	time    int
 }
 
-func AddMinutes() {
-
+func New(hrs, min int) Clock {
+	/////////////////////////////////////////
+	// hrs×60 = hours to minutes           //
+	// hrs×60+min = only minutes           //
+	// 24 = number of hours in a day       //
+	// 24×60 = number of minutes in a day  //
+	//-------------------------------------//
+	// modulo division(%) will help remove //
+	// extra hours and minutes for example //
+	//        25:74 = 26:14 = 02:14        //
+	/////////////////////////////////////////
+	var time int
+	time = ((hrs * 60) + min) % (24 * 60)
+	// need this if statement; % can give (-) result
+	if time < 0 {
+		time += 1440
+	}
+	return Clock{time: time}
 }
 
-func SubtractMinutes() {
-
+func (timepiece Clock) Add(min int) Clock {
+	var time int
+	time = (timepiece.time + min) % (24 * 60)
+	// need this if statement; % can give (-) result
+	if time < 0 {
+		time += 1440
+	}
+	return Clock{time: time}
 }
 
-func CompareClocks() {
-
+func (timepiece Clock) Subtract(min int) Clock {
+	var time int
+	time = (timepiece.time - min) % (24 * 60)
+	// need this if statement; % can give (-) result
+	if time < 0 {
+		time += 1440
+	}
+	return Clock{time: time}
 }
+
+func (timepiece Clock) String() string {
+	// Digital() function takes in a time
+	// converts it to digital time format
+	// then returns that formatted string
+
+	var time int
+	var min int
+	var hrs int
+	var digital string
+
+	time = timepiece.time % (24 * 60)
+	// need this if statement; % can give (-) result
+	if time < 0 {
+		time += 1440
+	}
+
+	min = time % 60
+	hrs = time / 60
+
+	if hrs < 10 {
+		digital += "0" + strconv.Itoa(hrs) + ":"
+	} else {
+		digital += strconv.Itoa(hrs) + ":"
+	}
+
+	if min < 10 {
+		digital += "0" + strconv.Itoa(min)
+	} else {
+		digital += strconv.Itoa(min)
+	}
+
+	return digital
+}
+
+// func CompareClocks() {
+
+// }
+
+// wanted to see solutions to help learning process — this project is *not* complete
