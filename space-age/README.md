589c282fbe723698e5ee18db9674dadd202dfaa5
Nolan Kovacik <noltron000@gmail.com>
2019-02-26 15:06:00 -0800

    🌎 space age go

--- space-age/README.md
+++ space-age/README.md
@@ -28,7 +28,6 @@ One way to figure out what the function signature(s) you would need is to look
 at the corresponding \*\_test.go file. It will show you what the package level
 functions(s) should be that the test will use to verify the solution.
 
-
 ## Running the tests
 
 To run the tests run the command `go test` from within the exercise directory.
@@ -36,7 +35,7 @@ To run the tests run the command `go test` from within the exercise directory.
 If the test suite contains benchmarks, you can run these with the `--bench` and `--benchmem`
 flags:
 
-    go test -v --bench . --benchmem
+> `go test -v --bench . --benchmem`
 
 Keep in mind that each reviewer will run benchmarks on a different machine, with
 different specs, so the results from these benchmark tests may vary.
---
+++ space-age/space_age.go
@@ -0,0 +1,90 @@
+package space
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+func convertUnit(time float64, unit_in string, unit_out string) (float64, error) {
+	unit_in = strings.ToLower(unit_in)
+	unit_out = strings.ToLower(unit_out)
+	var mult float64
+	var divi float64
+	var err error
+
+	switch unit_in {
+	case "seconds":
+		divi = 1.0 * 12 * (365 / 12) * 24 * 60 * 60
+	case "minutes":
+		divi = 1.0 * 12 * (365 / 12) * 24 * 60
+	case "hours":
+		divi = 1.0 * 12 * (365 / 12) * 24
+	case "days":
+		divi = 1.0 * 12 * (365 / 12)
+	case "months":
+		divi = 1.0 * 12
+	case "years":
+		divi = 1.0
+	default:
+		err = errors.New("That isn't a valid unit!")
+	}
+
+	switch unit_out {
+	case "seconds":
+		mult = 1.0 * 12 * (365.25 / 12) * 24 * 60 * 60
+	case "minutes":
+		mult = 1.0 * 12 * (365.25 / 12) * 24 * 60
+	case "hours":
+		mult = 1.0 * 12 * (365.25 / 12) * 24
+	case "days":
+		mult = 1.0 * 12 * (365.25 / 12)
+	case "months":
+		mult = 1.0 * 12
+	case "years":
+		mult = 1.0
+	default:
+		err = errors.New("That isn't a valid unit!")
+	}
+
+	return time * mult / divi, err
+}
+
+func convertAge(age float64, planet string) (float64, error) {
+	planet = strings.ToLower(planet)
+	var mult float64
+	var err error
+
+	fmt.Println(planet)
+
+	switch planet {
+	case "earth":
+		mult = 1.0
+	case "mercury":
+		mult = 0.2408467
+	case "venus":
+		mult = 0.61519726
+	case "mars":
+		mult = 1.8808158
+	case "jupiter":
+		mult = 11.862615
+	case "saturn":
+		mult = 29.447498
+	case "uranus":
+		mult = 84.016846
+	case "neptune":
+		mult = 164.79132
+	default:
+		err = errors.New("That's not a valid planet!")
+	}
+
+	return age / mult, err
+}
+
+func Age(seconds float64, planet string) float64 {
+	// var new_unit, _ = convertUnit(seconds, "seconds", "years")
+	// ↑this↑ is actually more accurate than ↓this↓
+	var new_unit = seconds / 31557600
+	var new_age, _ = convertAge(new_unit, planet)
+	return new_age
+}
